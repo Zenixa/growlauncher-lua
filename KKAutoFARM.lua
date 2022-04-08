f4e1a90bc8452a98ae9618fa342618d746691e7c
Zenixa#7524 <103260658+Zenixa@users.noreply.github.com>
2022-04-08 14:13:28 +0100

    Update KKAutoFARM.lua

--- KKAutoFARM.lua
+++ KKAutoFARM.lua
@@ -1,11 +1,9 @@
+--Private Server usage: will autofarm in the direction you are facing 3blocks bfg style
+
 itemID = FindItemID("Magplant 5000 Remote")
 
 placedelay = 110
 breakdelay = 110
-
-ecstasy = false
-ow = false
-mbfg = false
 
 --dont modify this ↓
 left = false
@@ -54,33 +52,6 @@ return item.amount
     end
     return 0
 end
-
---[[
-if(GetTiles(3,50).fg==FindItemID("MAGPLANT 5000")) then
-ecstasy = true
-SendPacket(2, "action|dialog_return\ndialog_name|magplant_edit\nx|3|\ny|50|\nbuttonClicked|getRemote")
-Sleep(500)
-elseif(GetTiles(4,23).fg==FindItemID("MAGPLANT 5000"))then
-ow = true
-SendPacket(2, "action|dialog_return\ndialog_name|magplant_edit\nx|3|\ny|50|\nbuttonClicked|getRemote")
-Sleep(500)
-elseif(GetTiles(4,23).fg==FindItemID("MAGPLANT 5000"))then
-mbfg = true
-SendPacket(2, "action|dialog_return\ndialog_name|magplant_edit\nx|3|\ny|50|\nbuttonClicked|getRemote")
-Sleep(500)
-end
-]]
-
---[[
-if(checkamount(FindItemID("MAGPLANT 5000 Remote")) == 0)then
-if(ecstasy == true)then
-SendPacket(2, "action|dialog_return\ndialog_name|magplant_edit\nx|"..tilex.."|\ny|"..tiley.."|\nbuttonClicked|getRemote")
-elseif(0w == true)then
-SendPacket(2, "action|dialog_return\ndialog_name|magplant_edit\nx|"..tilex.."|\ny|"..tiley.."|\nbuttonClicked|getRemote")
-elseif(mbfg == true)then
-SendPacket(2, "action|dialog_return\ndialog_name|magplant_edit\nx|"..tilex.."|\ny|"..tiley.."|\nbuttonClicked|getRemote")
-end
-]]
 
 if(GetLocal().isLeft == true)then
 left = true
