c0b825ccccbe96174dd67be13213bdedbf36d21c
Zenixa#7524 <103260658+Zenixa@users.noreply.github.com>
2022-04-08 14:04:37 +0100

    Update GrowscanPUBLIC.lua

--- GrowscanPUBLIC.lua
+++ GrowscanPUBLIC.lua
@@ -1,21 +1,8 @@
-function FindPathToObj(ItemID)
-for _, obj in pairs(GetObjectList()) do
-if obj.itemid == ItemID then
-FindPath(obj.posX / 32, obj.posY / 32)
-Sleep(1000)
-end
-end
-end
-
---FindPathToObj(FindItemID("World Lock")) -- 2 = ID Dirt, So FindPath To Dropped Dirt In World
-
-
 for _, obj in pairs(GetObjectList()) do
 if(obj.itemid == 0)then
-SendPacket(2, "action|input\n|text|`9Karva Noob`w: Nothing is here.")
 end
 Sleep(850)
-SendPacket(2, "action|input\n|text|`9SexHook`w: Found ID:"..obj.itemid.." Amount: "..obj.amount..".")
+SendPacket(2, "action|input\n|text|`9GSCAN`w: Found ID:"..obj.itemid.." Amount: "..obj.amount..".")
 end
 Sleep(850)
 SendPacket(2, "action|input\n|text|`9GSCAN`w: Finished")
